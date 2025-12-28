1e75869a3cc483ae8efb46f470b0a3395ec8c952
Vindhya <vindhyalakshmiofficial@gmail.com>
2025-12-28 17:06:53 +0530

    mongodb connected

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 import path from "path"
 import { ENV } from "./lib/env.js"
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
@@ -9,25 +10,39 @@ const app = express();
 
 const __dirname = path.resolve()
 
-
-
-app.get("/health", (req , res) => {
-    res.status(200).json({msg: "api is running"})
+app.get("/health", (req, res) => {
+    res.status(200).json({ msg: "api is running" })
 });
 
 
-app.get("/book", (req , res) => {
-    res.status(200).json({msg: "this is the books endpoint"})
+app.get("/book", (req, res) => {
+    res.status(200).json({ msg: "this is the books endpoint" })
 });
 
 // make our app ready for deployment
-if(ENV.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,"../client/dist")))
+if (ENV.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/dist")))
 
-    app.get("/{*any)", (req,res) => {
-        res.sendFile(path.join(__dirname,"../client", "dist", "index.html"));
+    app.get("/{*any)", (req, res) => {
+        res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
     })
 
 }
 
-app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT))
+
+
+
+
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(ENV.PORT, () =>
+            console.log("Server is running on port:", ENV.PORT));
+
+    } catch (error) {
+        console.error("Error starting the server", error)
+    }
+};
+
+startServer();
+
