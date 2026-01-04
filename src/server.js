859a75c16e6e3475ddfb7ae67766a3067c5ec813
Vindhya <vindhyalakshmiofficial@gmail.com>
2026-01-04 20:28:12 +0530

    env.js created

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import path from "path"
+import cors from "cors"
 import { ENV } from "./lib/env.js"
 import { connectDB } from "./lib/db.js";
 
@@ -9,6 +10,12 @@ dotenv.config();
 const app = express();
 
 const __dirname = path.resolve()
+
+//middleware
+app.use(express.json())
+//credentials:true meaning? => server allows a browser to include cookies on reuest
+app.use(cors({origin:ENV_CLIENT_URL,credential:true}))
+
 
 app.get("/health", (req, res) => {
     res.status(200).json({ msg: "api is running" })
