e3d8b0f957235903d1d242e8c9edc6090185dad9
Vindhya <vindhyalakshmiofficial@gmail.com>
2026-01-07 21:59:01 +0530

    clerk and inngest

--- src/server.js
+++ src/server.js
@@ -2,8 +2,11 @@ import express from "express"
 import dotenv from "dotenv"
 import path from "path"
 import cors from "cors"
+import { serve } from "inngest/express"
 import { ENV } from "./lib/env.js"
 import { connectDB } from "./lib/db.js";
+import { inngest } from "./lib/inngest.js"
+
 
 dotenv.config();
 
@@ -15,6 +18,8 @@ const __dirname = path.resolve()
 app.use(express.json())
 //credentials:true meaning? => server allows a browser to include cookies on reuest
 app.use(cors({origin:ENV_CLIENT_URL,credential:true}))
+
+app.use("/api/inngest", serve({client:inngest, functions}))
 
 
 app.get("/health", (req, res) => {
