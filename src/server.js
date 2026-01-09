91ea0fbaf38b346214268ae6289d44bedab2e8a1
Vindhya <vindhyalakshmiofficial@gmail.com>
2026-01-09 22:08:55 +0530

    protectrouter.js

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv"
 import path from "path"
 import cors from "cors"
 import { serve } from "inngest/express"
+import { clerkMiddleware } from '@clerk/express'
+
 import { ENV } from "./lib/env.js"
 import { connectDB } from "./lib/db.js";
 import { inngest, functions } from "./lib/inngest.js"
@@ -18,17 +20,24 @@ const __dirname = path.resolve()
 app.use(express.json())
 //credentials:true meaning? => server allows a browser to include cookies on reuest
 app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
+app.use(clerkMiddleware()); // this adds with field to request object: req.auth()
 
 app.use("/api/inngest", serve({client:inngest, functions}))
 
-
 app.get("/health", (req, res) => {
+    req.auth
     res.status(200).json({ msg: "api is running" })
 });
 
 
 app.get("/book", (req, res) => {
     res.status(200).json({ msg: "this is the books endpoint" })
+});
+
+//when you pass an array of middleware to express, it automatically flatterns and executes them sequentially, one by one
+app.get("/video-calls",protectRoute, (req, res) => {
+    
+    res.status(200).json({ msg: "this is protected route" })
 });
 
 // make our app ready for deployment
